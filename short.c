03312ee882a3dfd505b3f7a6db2e8b7c945f4d6c
Shun122634 <shun12263414@icloud.com>
2025-10-24 19:25:40 +0900

    Add files via upload

---
+++ short.c
@@ -0,0 +1,15 @@
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+
+
+
+int main()
+{
+    printf("short型の最大値: %d\n",SHRT_MAX);
+    printf("short型の最小値: %d\n", SHRT_MIN);
+    printf("short型の符号なし最大値: %d\n", USHRT_MAX);
+    
+
+    return 0;
+}
